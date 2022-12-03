7391a193df67959fb7398b20a5aa90c76a5bb244
Mohamad Homsi <homsimohamad4@gmail.com>
2022-12-03 16:39:25 +0200

    Fixed Some things

--- baked/src/Views/Home/Home.jsx
+++ baked/src/Views/Home/Home.jsx
@@ -8,46 +8,33 @@ import bakerImage from "../../Assets/meetTheBaker.svg";
 export default function Home() {
   return (
     <div>
-      <header>
-        <div
-          className="head"
-          style={{
-            backgroundImage: `url(${banner1})`,
-            backgroundRepeat: "no-repeat",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            width: "100%",
-            height: "100%",
-            zIndex: "0",
-          }}
-        >
-          {/* textContaner for the header or banner 1 at the top */}
-          <div className="textContainer">
-            <h1 className="Title">Hey, Sugar!</h1>
-            <h3 className="underTheTitle">Give Our Cookies a Try!</h3>
-            {/* button  */}
-            <button className="tryNowbtn">TRY NOW</button>
-          </div>
+      <div className="head">
+        <img src={banner1} alt="banner1" className="headImage" />
+        <div className="textContainer">
+          <h1 className="Title">Hey, Sugar!</h1>
+          <h3 className="underTheTitle">Give Our Cookies a Try!</h3>
+          <button className="tryNowbtn">TRY NOW</button>
         </div>
-        {/* the rest of the body */}
-        <div className="container">
-          {/* meet the baker part */}
-          <div className="meetTheBaker">
+      </div>
+      <div className="containerMeet">
+        <div className="meetTheBaker">
+          <div className="ImageWithBackground">
             <img src={bakerImage} alt="bakers img" className="bakerImage" />
             <div className="yellowBlock"></div>
-            <div className="bakerText">
-              <h1 className="bakerTitle">Meet the Baker</h1>
-              <div className="blackLine"></div>
-              <p className="bakerDescription">
-                Hello! I am Celina, I founded baked because I had a passion for
-                baking ever since I was 16. Ironically enough, I do not like
-                sweets, but I am passionate about baking, and I love seeing
-                people go crazy over what I dough.
-              </p>
-            </div>
+          </div>
+          <div className="bakerText">
+            <h1 className="bakerTitle">Meet the Baker</h1>
+            <div className="blackLine"></div>
+            <p className="bakerDescription">
+              Hello! I am Celina, I founded baked because I had a passion for
+              baking ever since I was 16. Ironically enough, I do not like
+              sweets, but I am passionate about baking, and I love seeing people
+              go crazy over what I dough.
+            </p>
+            <button className="tryNowbtnYellow">TRY NOW</button>
           </div>
         </div>
-      </header>
+      </div>
     </div>
   );
 }
