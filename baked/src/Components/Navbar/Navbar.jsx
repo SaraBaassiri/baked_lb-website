ed0c0cb5f2cf5d7a69ceb06e3dcdf4cd3fb15acc
Mohamad Homsi <homsimohamad4@gmail.com>
2022-12-03 15:48:05 +0200

    ..

--- baked/src/Components/Navbar/Navbar.jsx
+++ baked/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Navbar.css";
 import Logo from "../../Assets/Logo.svg";
+import { BsSearch, BsCartDash } from "react-icons/bs";
+import { FaRegUserCircle } from "react-icons/fa";
 
 export default function Navbar() {
   return (
@@ -13,6 +15,16 @@ export default function Navbar() {
           <a href="/">Home</a>
           <a href="/">Shop</a>
           <a href="/">Contact Us</a>
+        </div>
+        <div className="SearchBar">
+          <input type="text" placeholder="Chocolate cookie" />
+          <div className="SearchIcon">
+            <BsSearch />
+          </div>
+        </div>
+        <div className="navbarIcons">
+          <FaRegUserCircle size={35} />
+          <BsCartDash size={35} />
         </div>
       </div>
     </div>
