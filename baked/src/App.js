5f074ab97b298883a43cf71ca883abe8b82d3ebd
Mohamad Homsi <homsimohamad4@gmail.com>
2022-12-03 22:28:26 +0200

    Fixed Some things

--- baked/src/App.js
+++ baked/src/App.js
@@ -2,8 +2,19 @@ import Home from "./Views/Home/Home";
 import About from "./Views/About/About";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import React from "react";
 
 function App() {
+  React.useEffect(() => {
+    if (document.location.pathname !== "/") {
+      let title = "Baked - " +
+        document.location.pathname.split("/")[1].charAt(0).toUpperCase()
+        + document.location.pathname.split("/")[1].slice(1);
+      document.title = title;
+    }
+  });
+
   return (
     <div className="App">
       <Navbar />
@@ -11,6 +22,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
---
+++ baked/src/Components/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Footer.css";
+import Logo from "../../Assets/Logo.svg";
+import { BsInstagram, BsFacebook } from "react-icons/bs";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <div className="Logo">
+        <img src={Logo} alt="logo" />
+        <h3>©2022 Baked Lebanon</h3>
+      </div>
+      <div className="BakedInfo">
+        <h2>Baked</h2>
+        <p>
+          We serve quality desserts in Lebanon. All our bakeries are
+          professionally made from scratch using only the finest ingredients.
+        </p>
+      </div>
+      <div className="Socials">
+        <h2>Follow Our Socials</h2>
+        <div>
+          <BsFacebook size={35} />
+          <BsInstagram size={35} />
+        </div>
+      </div>
+      <div className="Links">
+        <h2>Quick Links</h2>
+        <div>
+          <a href="/">Home</a>
+          <a href="/">Shop</a>
+          <a href="/">Meet the Baker</a>
+          <a href="/">Products</a>
+          <a href="/">Best Sellers</a>
+          <a href="/">Customize Now</a>
+        </div>
+      </div>
+      <div className="GetUpdated">
+        <h2>Always get Updated</h2>
+        <p>
+          Join our mailing for updates on new product launches and seasonal
+          specials!
+        </p>
+        <input type="text" placeholder="Enter Email" />
+      </div>
+    </div>
+  );
+}
