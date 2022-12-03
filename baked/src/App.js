a07bf3f8d49eb4188383bf272dd8aa6916fd8f4c
Mohamad Homsi <homsimohamad4@gmail.com>
2022-12-03 14:27:02 +0200

    Added Routes

--- baked/src/App.js
+++ baked/src/App.js
@@ -1,10 +1,16 @@
+import Home from "./Views/Home/Home";
+import About from "./Views/About/About";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
