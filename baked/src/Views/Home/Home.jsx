5075c704d9a855445ccfc81e90161a0f06e6f00d
Mohamad Homsi <homsimohamad4@gmail.com>
2022-12-03 21:42:36 +0200

    ..

--- baked/src/Views/Home/Home.jsx
+++ baked/src/Views/Home/Home.jsx
@@ -8,31 +8,43 @@ import bakerImage from "../../Assets/meetTheBaker.svg";
 export default function Home() {
   return (
     <div>
-      <div className="head">
-        <img src={banner1} alt="banner1" className="headImage" />
-        <div className="textContainer">
-          <h1 className="Title">Hey, Sugar!</h1>
-          <h3 className="underTheTitle">Give Our Cookies a Try!</h3>
-          <button className="tryNowbtn">TRY NOW</button>
+      <div className="Hero">
+        <div className="HeroImage">
+          <img src={banner1} alt="banner1" />
+        </div>
+        <div className="HeroInner">
+          <div className="HeroText">
+            <h1 className="Hero-Title">Hey, Sugar!</h1>
+            <h3 className="Hero-SubTitle">Give Our Cookies a Try!</h3>
+            <button className="HeroButton">TRY NOW</button>
+          </div>
         </div>
       </div>
-      <div className="containerMeet">
-        <div className="meetTheBaker">
-          <div className="ImageWithBackground">
-            <img src={bakerImage} alt="bakers img" className="bakerImage" />
-            <div className="yellowBlock"></div>
-          </div>
-          <div className="bakerText">
-            <h1 className="bakerTitle">Meet the Baker</h1>
-            <div className="blackLine"></div>
-            <p className="bakerDescription">
+
+      <div className="MeetTheBaker">
+        <div className="BakerImage">
+          <img src={bakerImage} alt="banner1" />
+          <span className="YellowBackground"></span>
+        </div>
+        <div className="BakerInner">
+          <div className="BakerText">
+            <h1 className="Baker-Title">Meet the Baker</h1>
+            <div className="BlackLine"></div>
+            <p className="Baker-Des">
               Hello! I am Celina, I founded baked because I had a passion for
               baking ever since I was 16. Ironically enough, I do not like
               sweets, but I am passionate about baking, and I love seeing people
               go crazy over what I dough.
             </p>
-            <button className="tryNowbtnYellow">TRY NOW</button>
+            <button className="BakerButton">TRY NOW</button>
           </div>
+        </div>
+      </div>
+
+      <div className="Products">
+        <div className="InnerProducts">
+          <h1>Products</h1>
+          <div className="ProductsLine"></div>
         </div>
       </div>
     </div>
