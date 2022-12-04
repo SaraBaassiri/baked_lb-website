86ebb94a50c5085daab056087158fe3a9fa3469f
GoStevve <fawzy_mouzaien@hotmail.com>
2022-12-05 01:53:54 +0200

    Products edit1

--- baked/src/Views/Home/Home.jsx
+++ baked/src/Views/Home/Home.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 import "./Home.css";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from "swiper";
+import "swiper/css/bundle";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 //Svg
 import banner1 from "../../Assets/banner1.svg";
@@ -40,13 +48,59 @@ export default function Home() {
           </div>
         </div>
       </div>
-
+    {/* Start of Products code */}
       <div className="Products">
         <div className="InnerProducts">
           <h1>Products</h1>
-          <div className="ProductsLine"></div>
+          <div >
+          <Swiper
+            slidesPerView={3}
+            spaceBetween={120}
+            slidesPerGroup={3}
+            loop={true}
+            loopFillGroupWithBlank={true}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+          >
+            <SwiperSlide classname="card">
+              <img classname="card-img" alt="" src="/brownies.png"></img>
+              <br></br>
+              {/* Slide 1 */}
+              </SwiperSlide>
+            <SwiperSlide>
+            <img classname="card-img1" alt="" src="/cupcake.png"></img>
+              {/* Slide 2 */}
+              </SwiperSlide>
+            <SwiperSlide>
+            <img classname="card-img2" alt="" src="/cinna.png"></img>
+              {/* Slide 3 */}
+              </SwiperSlide>
+            <SwiperSlide>
+            <img classname="card-img3" alt="" src="/cookies.png"></img>
+              {/* Slide 4 */}
+              </SwiperSlide>
+            <SwiperSlide>
+            <img classname="card-img4" alt="" src="/oreshki.png"></img>
+              {/* Slide 5 */}
+              </SwiperSlide>
+            <SwiperSlide>
+            <img classname="card-img5" alt="" src="/pinkstuff.png"></img>
+              {/* Slide 6 */}
+              </SwiperSlide>
+              {/* Incase we needed more than 6 products */}
+            {/* <SwiperSlide>Slide 7</SwiperSlide>
+            <SwiperSlide>Slide 8</SwiperSlide>
+            <SwiperSlide>Slide 9</SwiperSlide> */}
+          </Swiper>
+          </div>
         </div>
       </div>
+      {/* End of Products code */}
+      {/* ----------------------------------------------------------------------------- */}
     </div>
   );
 }
