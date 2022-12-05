d9f3820ba21005dfc4ea2d26755ef2087ab650ef
Mohamad Homsi <homsimohamad4@gmail.com>
2022-12-05 22:05:01 +0200

    Update Home.jsx

--- baked/src/Views/Home/Home.jsx
+++ baked/src/Views/Home/Home.jsx
@@ -250,8 +250,8 @@ export default function Home() {
               <img src="BlueCup.png" alt="Cupcake" />
               <h3>Cupcake</h3>
               <p id="desc">
-                A very smoochy buttered piece of cake, 
-                with a frosty topic that you would fall for.
+                A very smoochy buttered piece of cake, with a frosty topic that
+                you would fall for.
               </p>
               <p id="price">$5</p>
               <button id="buyNowBtn">Buy Now</button>
@@ -262,8 +262,8 @@ export default function Home() {
               <img src="RedCake.png" alt="Red Velvet Cookie" />
               <h3>Red Velvet Cookie</h3>
               <p id="desc">
-                A bright red velvet cookies filled with M&M’s, 
-                gooey melty dark chocolate chips and rich dark chunks.
+                A bright red velvet cookies filled with M&M’s, gooey melty dark
+                chocolate chips and rich dark chunks.
               </p>
               <p id="price">$2.5</p>
               <button id="buyNowBtn">Buy Now</button>
@@ -274,15 +274,15 @@ export default function Home() {
               <img src="MandM.png" alt="M&M" />
               <h3>M&M</h3>
               <p id="desc">
-                A very distinct colorful juicy piece of cake, 
-                as each piece reflects the taste of a yummy fruity element.
+                A very distinct colorful juicy piece of cake, as each piece
+                reflects the taste of a yummy fruity element.
               </p>
               <p id="price">$10</p>
               <button id="buyNowBtn">Buy Now</button>
             </div>
           </Grid>
         </Grid>
-        
+
         <div className="BottomDiv">
           <h2>Taking your celebration to a whole new level!</h2>
           <button className="HeroButton">View All</button>
@@ -295,13 +295,13 @@ export default function Home() {
         <h1>Celebrating Special Occasions?</h1>
         <Grid
           container
-          spacing={3}
-          columns={24}
+          spacing={15}
+          columns={18}
           alignItems="center"
           justify="center"
           style={{ minHeight: "100vh", paddingInline: "5%" }}
         >
-          <Grid item xs={8}>
+          <Grid item xs={6}>
             <div className="item">
               <img src={Pumpkin} alt="" />
               <h3>Pumpkin Pie</h3>
@@ -311,7 +311,7 @@ export default function Home() {
               </p>
             </div>
           </Grid>
-          <Grid item xs={8}>
+          <Grid item xs={6}>
             <div className="item">
               <img src={ChrisBrownies} alt="" />
               <h3>Christmas Brownies</h3>
@@ -321,7 +321,7 @@ export default function Home() {
               </p>
             </div>
           </Grid>
-          <Grid item xs={8}>
+          <Grid item xs={6}>
             <div className="item">
               <img src={IndependanceDay} alt="" />
               <h3>Independence Day</h3>
