0ce98047620ab7e972624a3fa8940a7bc750c745
SaraBaassiri <sarabsr95@gmail.com>
2022-12-08 17:15:30 +0300

    tried to fix

--- src/Views/Shop/Shop.jsx
+++ src/Views/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Shop.css";
 import { Grid } from "@mui/material";
 
+
 export default function Shop() {
   return (
     <div className="shop">
@@ -13,14 +14,35 @@ export default function Shop() {
         {Array.from(Array(8)).map((_, index) => (
           <Grid item xs={2} sm={4} md={4} key={index}>
             <div className="ShopItem">
-              <div className="ShopInnerImage">
-                <img src="/cupcake2.png" alt="" />
+              <div className="ShopInnerImage1">
+                <img src="/BlueCup.png" alt="" />
                 <span className="InnerBackground" />
               </div>
               <h2>Cupcake</h2>
               <p>
                 A very smoochy buttered piece of cake, with a frosty topic that
                 you would fall for.
+              </p>
+              <h3 className="price">${index}</h3>
+              <div className="LowerItemSection">
+                <div className="AddItem">
+                  <p>-</p>
+                  <p>{index}</p>
+                  <p>+</p>
+                </div>
+                <button>Buy Now</button>
+              </div>
+            </div>
+
+            <div className="ShopItem">
+              <div className="ShopInnerImage2">
+                <img src="/RedCake.png" alt="" />
+                <span className="InnerBackground" />
+              </div>
+              <h2>Red Velvet Cookie</h2>
+              <p>
+                A bright red velvet cookies filled with M&M’s, 
+                gooey melty dark chocolate chips and rich dark chunks.
               </p>
               <h3>{index}$</h3>
               <div className="LowerItemSection">
@@ -29,7 +51,7 @@ export default function Shop() {
                   <p>{index}</p>
                   <p>+</p>
                 </div>
-                <button>BUY NOW</button>
+                <button>Buy Now</button>
               </div>
             </div>
           </Grid>
