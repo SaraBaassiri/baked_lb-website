8662533b2ef32857b3113d1bbe0b0669a9f12840
Mohamad Homsi <homsimohamad4@gmail.com>
2022-12-08 21:38:40 +0200

    ..

--- src/Views/Shop/Shop.jsx
+++ src/Views/Shop/Shop.jsx
@@ -37,7 +37,6 @@ export default function Shop() {
           </div>
         </Grid>
 
-        {/* New Item */}
         <Grid item xs={2} sm={4} md={4}>
           <div className="ShopItem">
             <div className="ShopInnerImage2">
@@ -69,8 +68,8 @@ export default function Shop() {
             </div>
             <h2>Macrons</h2>
             <p>
-              A very distinct colorful juicy piece of cake, 
-              as each piece reflects the taste of a yummy fruity element.
+              A very distinct colorful juicy piece of cake, as each piece
+              reflects the taste of a yummy fruity element.
             </p>
             <h3 className="price">$10</h3>
             <div className="LowerItemSection">
@@ -92,8 +91,8 @@ export default function Shop() {
             </div>
             <h2>Rainbow Cake</h2>
             <p>
-              A very distinct colorful juicy piece of cake, 
-              as each piece reflects the taste of a yummy fruity element.
+              A very distinct colorful juicy piece of cake, as each piece
+              reflects the taste of a yummy fruity element.
             </p>
             <h3 className="price">$10</h3>
             <div className="LowerItemSection">
@@ -115,8 +114,8 @@ export default function Shop() {
             </div>
             <h2>Cinnamon Roll</h2>
             <p>
-              A very smoochy buttered piece of cake, 
-              with a frosty topic that you would fall for.
+              A very smoochy buttered piece of cake, with a frosty topic that
+              you would fall for.
             </p>
             <h3 className="price">$5</h3>
             <div className="LowerItemSection">
@@ -138,8 +137,8 @@ export default function Shop() {
             </div>
             <h2>Fudge Brownie</h2>
             <p>
-              A bright red velvet cookies filled with M&M’s, 
-              gooey melty dark chocolate chips and rich dark chunks.
+              A bright red velvet cookies filled with M&M’s, gooey melty dark
+              chocolate chips and rich dark chunks.
             </p>
             <h3 className="price">$2.5</h3>
             <div className="LowerItemSection">
@@ -152,8 +151,50 @@ export default function Shop() {
             </div>
           </div>
         </Grid>
-
-        {/* End new Item */}
+        <Grid item xs={2} sm={4} md={4}>
+          <div className="ShopItem">
+            <div className="ShopInnerImage1">
+              <img src="/chocoChip.png" alt="Fudge Brownie" />
+              <span className="InnerBackground" />
+            </div>
+            <h2>Fudge Brownie</h2>
+            <p>
+              A bright red velvet cookies filled with M&M’s, gooey melty dark
+              chocolate chips and rich dark chunks.
+            </p>
+            <h3 className="price">$2.5</h3>
+            <div className="LowerItemSection">
+              <div className="AddItem">
+                <p>-</p>
+                <p>0</p>
+                <p>+</p>
+              </div>
+              <button>Buy Now</button>
+            </div>
+          </div>
+        </Grid>
+        <Grid item xs={2} sm={4} md={4}>
+          <div className="ShopItem">
+            <div className="ShopInnerImage3">
+              <img src="/macron.png" alt="Fudge Brownie" />
+              <span className="InnerBackground" />
+            </div>
+            <h2>Fudge Brownie</h2>
+            <p>
+              A bright red velvet cookies filled with M&M’s, gooey melty dark
+              chocolate chips and rich dark chunks.
+            </p>
+            <h3 className="price">$2.5</h3>
+            <div className="LowerItemSection">
+              <div className="AddItem">
+                <p>-</p>
+                <p>0</p>
+                <p>+</p>
+              </div>
+              <button>Buy Now</button>
+            </div>
+          </div>
+        </Grid>
       </Grid>
     </div>
   );
