099a228673c77d775f610f1c459773887d9f0983
GoStevve <fawzy_mouzaien@hotmail.com>
2022-12-15 08:18:28 +0200

    register draft 1

--- src/Components/popups/auth.jsx
+++ src/Components/popups/auth.jsx
@@ -10,8 +10,10 @@ class Auth extends Component {
                 style={{
                     position: "absolute",
                     backgroundColor: "#E0D2F5",
-                    width: "590px",
-                    height: "580px",
+                    width: "auto",
+                    height: "auto",
+                    // width: "590px",
+                    // height: "580px",
                     borderRadius: "69px",
                     borderWidth:"1px",
                     left: "50%",
@@ -26,9 +28,117 @@ class Auth extends Component {
             >
                 <div className="everything">
                     <h1 className="titleR">REGISTER</h1>
-                    <div className="Line"></div>
-                    <p className="cout2">Contact the help team</p>
-                
+                    <div className="titleRline"></div>
+                    <p className="cout2">Register to have the earliest discounts, and latest products</p>
+
+                    <div className="grid-container1">
+                        <div className="grid-child">
+                            <p className="label">First Name*</p>
+                        </div>
+                        <div className="grid-child">
+                            <p className="label">Last Name*</p>
+                        </div>
+                    </div>
+
+                    <div className="grid-containerb">
+                        <div className="grid-child">
+                            <label>
+                                <input
+                                    type="text"
+                                    className="inputbox2"
+                                    name="firstname"
+                                    placeholder="First Name"
+                                />
+                            </label>
+                        </div>
+                        <div className="grid-child">
+                            <label>
+                                <input
+                                    type="text"
+                                    className="inputbox2"
+                                    name="lastname"
+                                    placeholder="Last Name"
+                                />
+                                </label>
+                        </div>
+                    </div>
+
+                    <div className="grid-container1">
+                        <div className="grid-child">
+                            <p className="label">Phone Number*</p>
+                        </div>
+                        <div className="grid-child">
+                            <p className="label">Email*</p>
+                        </div>
+                    </div>
+
+                    <div className="grid-containerb">
+                        <div className="grid-child">
+                            <label>
+                                <input
+                                    type="text"
+                                    className="inputbox2 short"
+                                    name="phonenumber"
+                                    placeholder="Phone Number"
+                                />
+                            </label>
+                        </div>
+                        <div className="grid-child">
+                            <label>
+                                <input
+                                    type="text"
+                                    className="inputbox2long"
+                                    name="email"
+                                    placeholder="Email"
+                                />
+                            </label>
+                        </div>
+                    </div>
+
+                    <div className="grid-container1">
+                        <div className="grid-child">
+                            <p className="label">Password*</p>
+                        </div>
+                        <div className="grid-child">
+                            <p className="label">Confirm Password*</p>
+                        </div>
+                    </div>
+
+                    <div className="grid-containerb">
+                        <div className="grid-child">
+                            <label>
+                                <input
+                                    type="password"
+                                    className="inputbox2"
+                                    name="password"
+                                    placeholder="Password"
+                                />
+                            </label>
+                        </div>
+                        <div className="grid-child">
+                            <label>
+                                <input
+                                    type="password"
+                                    className="inputbox2long"
+                                    name="cpassword"
+                                    placeholder="Confirm Password"
+                                />
+                            </label>
+                        </div>
+                    </div>
+                    <div className="check">
+                        <label>
+                            <input type="checkbox" />
+                            Yes, I want to subscribe to get informed of any new products
+                        </label>
+                    </div>
+                    <br />
+                    <div className="check">
+                    <label>
+                        <input type="checkbox" />
+                        I agree to all the <b>Terms, Privacy,</b> and <b>Fees</b>
+                    </label>
+                    </div>
                 </div>
             </div>
         );
